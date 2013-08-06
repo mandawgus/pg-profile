1e09bca972e9057147789de5400f50218c345f99
Dirty Sauce <de.x412@gmail.com>
2013-08-06 04:06:31 -0400

    converted to backbone/marionette

--- index.php
+++ index.php
@@ -10,236 +10,186 @@
 <body>
 	<div id="app">
 
-		<div class="navbar">
-			<div class="container">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-
-				<!-- Be sure to leave the brand out there if you want it shown -->
-				<a class="navbar-brand" href="#">My PG Profile</a>
-
-				<!-- Place everything within .navbar-collapse to hide it until above 768px -->
-				<div class="nav-collapse collapse navbar-responsive-collapse">
-					<ul class="nav navbar-nav visible-sm">
-						<li class="active"><a href="#">My Profile</a></li>
-						<li><a href="#">Change Password</a></li>
-						<li><a href="#">Account History</a></li>
-						<li><a href="#">Link Print Subscription</a></li>
-						<li><a href="#">Manage Credit Card</a></li>
-						<li><a href="#">Manage Subscription</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
-
+		<div id="nav-view"></div>
 
 		<div class="container">
 
 			<div id="side-view" class="col-lg-3 hidden-sm"></div>
-			<div id="main-view" class="col-lg-9">
-				<h1>My Profile</h1>
-				<div class="row">
-					<div class="col-lg-6">
-						<address>
-							<strong>Twitter, Inc.</strong><br>
-							795 Folsom Ave, Suite 600<br>
-							San Francisco, CA 94107<br>
-							<abbr title="Phone">P:</abbr> (123) 456-7890
-						</address>
-
-						<address>
-							<strong>Full Name</strong><br>
-							<a href="mailto:#">first.last@example.com</a>
-						</address>
-					</div>
-					<div class="col-lg-6">
-						<button class="btn btn-primary">Edit my profile</button>
-					</div>
-				</div>
-
-				<hr />
-
-				<h1>Change Password</h1>
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="form-group">
-							<input id="original" type="password" class="form-control" placeholder="Original Password">
-						</div>
-						<div class="form-group">
-							<input id="new" type="password" class="form-control" placeholder="New Password">
-						</div>
-						<div class="form-group">
-							<input id="confirm" type="password" class="form-control" placeholder="Retype New Password">
-						</div>
-						<div class="form-group">
-							<button class="btn btn-primary">Submit</button>
-						</div>
-					</div>
-					<div class="col-lg-6"></div>
-				</div>
-
-				<hr />
-
-				<h1>Account History</h1>
-				<div class="row">
-					<div class="col-lg-12">
-						<table class="table table-condensed table-striped">
-							<thead>
-								<tr>
-									<th>ID #</th>
-									<th>Description</th>
-									<th>Something Else</th>
-									<th>Date</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>1</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>2</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>3</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>4</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>5</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>6</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>7</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-								<tr>
-									<td>8</td>
-									<td>Monthly Subscription: Recurring Monthly</td>
-									<td>$9.99</td>
-									<td>xx/xx/xxxx</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-
-				<hr />
-
-				<h1>My Payment Options</h1>
-				<div class="row">
-					<div class="col-lg-6">
-						<h3>Credit Card Information</h3>
-						<address>
-							<strong>Firstname Lastname</strong><br>
-							Type: Visa<br>
-							**** **** **** **** 0002<br>
-							Exp: xx/xxxx
-						</address>
-
-						
-						<h3>EZ-PAY</h3>
-						<p>EZ-PAY is the simplest, most economical payment plan. By answering YES here, your payment will be automatically debited or credited to the card of your choice at renewal.
-						
-						<div class="form-group">
-							<input type="checkbox" value=""> Yes, enroll me in EZ-PAY
-						</div>
-
-						<div class="form-group">
-							<label>EZ-PAY billing cycle</label>
-							<select class="form-control">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
-						</div>
-
-						<div class="form-group">
-							<label>Day of the month payments occur</label>
-							<select class="form-control">
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-							</select>
-						</div>
-					</div>
-					<div class="col-lg-6">
-						<button class="btn btn-primary">Edit</button>
-					</div>
-				</div>
-
-				<hr />
-
-			</div>
+			<div id="main-view" class="col-lg-9"></div>
 		</div>
 
 	</div>
+
+	
+	<script type="text/template" id="navTemplate">
+		<div class="container">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+
+
+			<a class="navbar-brand" href="#">My PG Profile</a>
+
+			<div class="nav-collapse collapse navbar-responsive-collapse">
+				<% if (status){ %>
+				<p class="navbar-text pull-left">Signed in as <%= firstname %> <%= lastname %></p>
+				<ul class="nav navbar-nav visible-sm">
+					<li class="active"><a href="#">My Profile</a></li>
+					<li><a href="#">Change Password</a></li>
+					<li><a href="#">Account History</a></li>
+					<li><a href="#">My Credit Card Information</a></li>
+					<li><a href="#">Manage Subscription</a></li>
+					<li><a href="#">Link To My Print Subscription</a></li>
+				</ul>
+				<p class="navbar-text pull-right"><i class="icon-off"></i> <a id="logout" href="#" class="navbar-link">Logout</a></p>
+				<%}%>
+			</div>
+
+		</div>
+	</script>
 
 	<script type="text/template" id="sideTemplate">
 		<a id="profile" href="#" class="list-group-item active">My Profile</a>
 		<a id="password" href="#" class="list-group-item">Change Password</a>
 		<a id="history" href="#" class="list-group-item">Account History</a>
-		<a id="link" href="#" class="list-group-item">Link Print Subscription</a>
-		<a id="credit" href="#" class="list-group-item">Manage Credit Card</a>
-		<a id="subscription" href="#" class="list-group-item">Manage Subscription</a>
+		<a id="credit" href="#" class="list-group-item">My Credit Card Information</a>
+		<a id="subscription" href="#" class="list-group-item">Manage My Subscription</a>
+		<a id="link" href="#" class="list-group-item">Link To My Print Subscription</a>
 	</script>
 
 	<script type="text/template" id="profileTemplate">
-		<h1>My Profile</h1>
-		<div class="row">
-			<div class="col-lg-6">
-				<address>
-					<strong>Twitter, Inc.</strong><br>
-					795 Folsom Ave, Suite 600<br>
-					San Francisco, CA 94107<br>
-					<abbr title="Phone">P:</abbr> (123) 456-7890
-				</address>
+		<div class="col-lg-6">
+			<h1>My Profile</h1>
+			<address>
+				<strong>Twitter, Inc.</strong><br>
+				795 Folsom Ave, Suite 600<br>
+				San Francisco, CA 94107<br>
+				<abbr title="Phone">P:</abbr> (123) 456-7890
+			</address>
 
-				<address>
-					<strong>Full Name</strong><br>
-					<a href="mailto:#">first.last@example.com</a>
-				</address>
-			</div>
-			<div class="col-lg-6">
+			<address>
+				<strong>Full Name</strong><br>
+				<a href="mailto:#">first.last@example.com</a>
+			</address>
+
+			<div class="form-group">
 				<button class="btn btn-primary">Edit my profile</button>
 			</div>
 		</div>
+		<div class="col-lg-6"></div>
+	</script>
+
+	<script type="text/template" id="changePasswordTemplate">
+		<div class="col-lg-6">
+			<h1>Change Password</h1>
+			<div class="form-group">
+				<input id="original" type="password" class="form-control" placeholder="Original Password">
+			</div>
+			<div class="form-group">
+				<input id="new" type="password" class="form-control" placeholder="New Password">
+			</div>
+			<div class="form-group">
+				<input id="confirm" type="password" class="form-control" placeholder="Retype New Password">
+			</div>
+			<div class="form-group">
+				<button class="btn btn-primary">Submit</button>
+			</div>
+		</div>
+		<div class="col-lg-6"></div>
+	</script>
+
+	<script type="text/template" id="creditCardTemplate">
+		<div class="col-lg-6">
+			<h1>My Payment Options</h1>
+			<h3>Credit Card Information</h3>
+			<address>
+				<strong>Firstname Lastname</strong><br>
+				Type: Visa<br>
+				**** **** **** **** 0002<br>
+				Exp: xx/xxxx
+			</address>
+
+			<div class="form-group">
+				<button class="btn btn-primary">Edit Credit Card Details</button>
+			</div>
+			
+			<h3>EZ-PAY</h3>
+			<p>EZ-PAY is the simplest, most economical payment plan. By answering YES here, your payment will be automatically debited or credited to the card of your choice at renewal.
+			
+			<div class="form-group">
+				<input type="checkbox" value=""> Yes, enroll me in EZ-PAY
+			</div>
+
+			<div class="form-group">
+				<label>EZ-PAY billing cycle</label>
+				<select class="form-control">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+				</select>
+			</div>
+
+			<div class="form-group">
+				<label>Day of the month payments occur</label>
+				<select class="form-control">
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+				</select>
+			</div>
+		</div>
+		<div class="col-lg-6"></div>
+	</script>
+
+	<script type="text/template" id="subscriptionTemplate">
+		<%= planName %>
+	</script>
+
+	<script type="text/template" id="subscriptionsTemplate">
+		<div class="col-lg-6">
+			<h1>Subscription Template</h1>
+		</div>
+		<div class="col-lg-6"></div>
+	</script>
+
+
+
+	<script type="text/template" id="linkPrintTemplate">
+		<div class="col-6">
+			<h1>Link Print Subscription</h1>
+			<div class="form-group">
+				<input id="email" type="text" class="form-control" placeholder="Email Address">
+			</div>
+			<div class="form-group">
+				<input id="pass" type="password" class="form-control" placeholder="Password">
+			</div>
+			<div class="form-group">
+			<button id="loginBtn" class="btn btn-primary">Login</button>
+			</div>
+		</div>
+		<div class="col-6"></div>
 	</script>
 
 	<script type="text/template" id="loginTemplate">
-		<div class="form-group">
-			<input type="text" class="form-control" placeholder="Email Address">
+		<div class="col-6">
+			<h1>Login:</h1>
+			<div class="form-group">
+				<input id="email" type="text" class="form-control" placeholder="Email Address">
+			</div>
+			<div class="form-group">
+				<input id="pass" type="password" class="form-control" placeholder="Password">
+			</div>
+			<div class="form-group">
+			<button id="loginBtn" class="btn btn-primary">Login</button>
+			</div>
 		</div>
+		<div class="col-6"></div>
 	</script>
 
 	<script src="js/vendor/jquery-1.10.2.min.js"></script>
@@ -247,6 +197,7 @@
 	<script src="js/vendor/underscore.js"></script>
 	<script src="js/vendor/backbone.js"></script>
 	<script src="js/vendor/backbone.marionette.min.js"></script>
+	<script src="js/vendor/jquery.cookie.js"></script>
 	<script src="js/app/app.js"></script>
 
 </body>
--- js/app/app.js
+++ js/app/app.js
@@ -1,9 +1,10 @@
+
 var PGProfile = new Backbone.Marionette.Application({
 	navHome: function(){
 		PGProfile.router.navigate("", true);
 	},
 	navProfile: function(){
-		PGProfile.router.navigate("profile", true);
+		PGProfile.router.navigate("myProfile", true);
 	},
 	navPass: function(){
 		PGProfile.router.navigate("password", true);
@@ -19,11 +20,80 @@ var PGProfile = new Backbone.Marionette.Application({
 	},
 	navSub: function(){
 		PGProfile.router.navigate("subscription", true);
+	},
+	navLogout: function(){
+		PGProfile.router.navigate("logout", true);
 	}
 });
 
+var LoggedInStatus = Backbone.Model.extend({
+	defaults:{
+		status: false,
+		firstname: "",
+		lastname: ""
+	}
+});
+
+var Subscription = Backbone.Model.extend({});
+var Subscriptions = Backbone.Collection.extend({
+	model: Subscription,
+	url: 'https://stage.syncaccess.net/po/pit/api/svcs/availableplans?format=json'
+});
+
+
+
+
 var LoginView = Backbone.Marionette.ItemView.extend({
-	template: '#loginTemplate'
+	template: '#loginTemplate',
+	className: 'row',
+	events: {
+		'click #loginBtn' : 'loginUser'
+	},
+	initialize: function(){
+		var hasCookie = Boolean($.cookie('_pgp'));
+		if(hasCookie){
+			console.log("has Cookie");
+			this.loggedIn();
+		}
+	},
+	ui: {
+		email: '#email',
+		pass: '#pass'
+	},
+	removeAlerts: function(){
+
+	},
+	loginUser: function(){
+		console.log(this.ui.email.val(), this.ui.pass.val());
+		if (this.ui.email.val() === 'test' && this.ui.pass.val() === 'test'){
+			$.cookie('_pgp', "sessionid", {expires: 7, path: '/'});
+			this.loggedIn();
+		}
+	},
+	loggedIn: function(){
+		console.log("logged in");
+		PGProfile.loggedInModel.set({status: true, firstname: "Firstname", lastname: "Lastname"});
+		PGProfile.navProfile();
+	},
+	failedAttempt: function(){
+
+	}
+});
+var LoggedOutView = Backbone.View.extend({
+	template: '#navLoggedOut',
+	className: 'navbar'
+});
+
+var NavView = Backbone.Marionette.ItemView.extend({
+	template: '#navTemplate',
+	className: 'navbar',
+	events: {
+		'click #logout': 'logmeout'
+	},
+	logmeout: function(e){
+		e.preventDefault();
+		PGProfile.navLogout();
+	}
 });
 
 var SideView = Backbone.Marionette.ItemView.extend({
@@ -37,40 +107,71 @@ var SideView = Backbone.Marionette.ItemView.extend({
 		'click #credit' : 'showCredit',
 		'click #subscription' : 'showSubscription'
 	},
+	changeActive: function(element){
+		$(element).addClass('active').siblings().removeClass('active');
+	},
 	showProfile: function(e){
 		e.preventDefault();
 		PGProfile.navProfile();
+		this.changeActive(e.currentTarget);
 	},
 	showPassword: function(e){
 		e.preventDefault();
 		PGProfile.navPass();
+		this.changeActive(e.currentTarget);
 	},
 	showHistory: function(e){
 		e.preventDefault();
 		PGProfile.navHist();
+		this.changeActive(e.currentTarget);
 	},
 	showLink: function(e){
 		e.preventDefault();
 		PGProfile.navLink();
+		this.changeActive(e.currentTarget);
 	},
 	showCredit: function(e){
 		e.preventDefault();
 		PGProfile.navCredit();
+		this.changeActive(e.currentTarget);
 	},
 	showSubscription: function(e){
 		e.preventDefault();
 		PGProfile.navSub();
+		this.changeActive(e.currentTarget);
 	}
 });
 
 
 var ProfileView = Backbone.Marionette.ItemView.extend({
-	template: '#profileTemplate'
+	template: '#profileTemplate',
+	className: 'row'
 });
 
 
+var ChangePasswordView = Backbone.Marionette.ItemView.extend({
+	template: '#changePasswordTemplate',
+	className: 'row'
+});
 
+var CreditCardView = Backbone.Marionette.ItemView.extend({
+	template: '#creditCardTemplate',
+	className: 'row'
+});
 
+var SubscriptionView = Backbone.Marionette.ItemView.extend({
+	template: '#subscriptionTemplate'
+})
+
+var SubscriptionsView = Backbone.Marionette.CollectionView.extend({
+	className: 'row',
+	itemView: SubscriptionView
+});
+
+var LinkPrintView = Backbone.Marionette.ItemView.extend({
+	template: '#linkPrintTemplate',
+	className: 'row'
+});
 
 
 
@@ -82,11 +183,18 @@ PGProfile.addInitializer(function(){
 	});
 
 	//Data
+	PGProfile.subscriptions = new Subscriptions();
+	PGProfile.loggedInModel = new LoggedInStatus();
 
 	//Views
+	PGProfile.navLayout = new NavView({model: PGProfile.loggedInModel});
 	PGProfile.loginLayout = new LoginView();
 	PGProfile.sideLayout = new SideView();
 	PGProfile.profileLayout = new ProfileView();
+	PGProfile.changePassLayout = new ChangePasswordView();
+	PGProfile.creditCardLayout = new CreditCardView();
+	PGProfile.subscriptionsLayout = new SubscriptionsView({collection: PGProfile.subscriptions});
+	PGProfile.linkPrintLayout = new LinkPrintView({collection: PGProfile.loggedInStatus});
 
 	PGProfile.AppLayout = Marionette.Layout.extend({
 		regions: {
@@ -96,53 +204,86 @@ PGProfile.addInitializer(function(){
 		}
 	});
 
+	//PGProfile.SubscriptionsLayout = Marionette.Layout.extend({});
+
 	PGProfile.appLayout = new PGProfile.AppLayout({ el: '#app' });
 });
 
 PGProfile.on("initialize:after", function(){
-	Backbone.history.start({pushState: true, root: 'pg-profile'});
+	//Backbone.history.start({pushState: true, root: 'pg-profile'});
+	Backbone.history.start();
+	console.log("after:init");
+
 });
 
 PGProfile.Controller = Marionette.Controller.extend({
+	index: function(){
+		console.log(Boolean($.cookie('_pgp')));
+		console.log($.cookie('_pgp'));
+		PGProfile.appLayout.navView.show(PGProfile.navLayout);
+		if ($.cookie('_pgp')){
+			PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+			PGProfile.appLayout.mainView.show(PGProfile.profileLayout);
+
+		} else {
+			window.location.hash = '';
+			PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
+			return this;
+		}
+	},
 	login: function(){
-		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
+		//PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
 	},
 	myProfile: function(){
+		PGProfile.appLayout.navView.show(PGProfile.navLayout);
 		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
 		PGProfile.appLayout.mainView.show(PGProfile.profileLayout);
 	},
 	password: function(){
-		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		PGProfile.appLayout.mainView.show(PGProfile.changePassLayout);
+		//PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
 	},
 	accountHistory: function(){
 		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		//PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
 	},
 	linkAccount: function(){
-		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		PGProfile.appLayout.mainView.show(PGProfile.linkPrintLayout);
 	},
 	creditCard: function(){
-		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		PGProfile.appLayout.mainView.show(PGProfile.creditCardLayout);
+		//PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
 	},
 	subscription: function(){
-		//PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
-		PGProfile.appLayout.sideView.show(PGProfile.sideLayout);
+		PGProfile.appLayout.mainView.show(PGProfile.subscriptionsLayout);
+		//PGProfile.subscriptions.fetch();
+	},
+	logout: function(){
+		$.cookie('_pgp', "", {expires: 7, path: '/'});
+		PGProfile.loggedInModel.set({status: false, firstname: "", lastname: ""});
+		PGProfile.appLayout.sideView.close();
+		PGProfile.appLayout.mainView.show(PGProfile.loginLayout);
+		location.reload();
+		return this;
 	}
 });
 
 PGProfile.Router = Marionette.AppRouter.extend({
+	initialize: function(){
+		window.location.hash = '';
+		return this;
+	},
 	appRoutes: {
-		"": "login",
-		"profile": "myProfile",
+		"": "index",
+		"login": "login",
+		"myProfile": "myProfile",
 		"password": "password",
 		"history": "accountHistory",
 		"link": "linkAccount",
 		"cc": "creditCard",
-		"subscription": "subscription"
+		"subscription": "subscription",
+		"logout": "logout"
 	}
 });
 
